e05bb715ee15b7541e3583e44d7ba1ed6e7b15fd
Richard Levitte <richard@levitte.org>
2021-01-29 10:35:05 +0100

    Set up test directory, start with test_common.[ch] from vigenere

---
+++ test/test_common.c
@@ -0,0 +1,15 @@
+/* CC0 license applied */
+
+#include "test_common.h"
+
+void hexdump(const void *ptr, size_t len)
+{
+    const unsigned char *p = ptr;
+    size_t i, j;
+
+    for (i = 0; i < len; i += j) {
+        for (j = 0; j < 16 && i + j < len; j++)
+            printf("%s%02x", j? "" : " ", p[i + j]);
+    }
+    printf("\n");
+}

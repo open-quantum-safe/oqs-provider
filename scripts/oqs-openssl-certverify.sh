3513708a297f1a434b42c17a962e7e19379e27bf
Michael <57787676+baentsch@users.noreply.github.com>
2021-10-24 09:15:27 +0200

    CCI workaround

--- scripts/oqs-openssl-certverify.sh
+++ scripts/oqs-openssl-certverify.sh
@@ -19,4 +19,12 @@ if [ ! -f tmp/$1_srv.crt ]; then
     exit 1
 fi
 
+if [[ -z "$CIRCLECI" ]]; then
 docker run -v `pwd`/tmp:/home/oqs/data -it $IMAGE sh -c "cd /home/oqs/data && openssl verify -CAfile $1_CA.crt $1_srv.crt"
+else
+# CCI doesn't permit mounting, so let's do as per https://circleci.com/docs/2.0/building-docker-images/#mounting-folders:
+docker create -v /certs --name certs alpine /bin/true && \
+docker cp tmp/ certs:/certs && \
+docker run --volumes-from certs -it $IMAGE sh -c "cd /certs/tmp && openssl verify -CAfile $1_CA.crt $1_srv.crt" && \
+docker rm /certs
+fi

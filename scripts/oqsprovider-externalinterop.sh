8675edd133c4ef8085f0851aca0b82abcf10975d
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2023-10-17 08:07:00 +0200

    disable interop tests in proxied environments

--- scripts/oqsprovider-externalinterop.sh
+++ scripts/oqsprovider-externalinterop.sh
@@ -18,6 +18,12 @@ if [ -z "$DYLD_LIBRARY_PATH" ]; then
     export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
 fi
 
+# Disable this test if HTTP_PROXY or HTTPS_PROXY is set
+if [ -v HTTP_PROXY ] || [ -v HTTPS_PROXY ]; then
+    echo "HTTP(S) proxy vars set. Disabling interop test."
+    exit 0
+fi
+
 echo " Cloudflare:"
 export OQS_CODEPOINT_X25519_KYBER512=65072
 (echo -e "GET /cdn-cgi/trace HTTP/1.1\nHost: cloudflare.com\n\n"; sleep 1; echo $'\cc') | $OPENSSL_APP s_client -connect pq.cloudflareresearch.com:443 -groups x25519_kyber768 -servername cloudflare.com -ign_eof 2>/dev/null | grep kex=X25519Kyber768Draft00

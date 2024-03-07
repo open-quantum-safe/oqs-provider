3f6ecf928bf5c852bc30a7c463e62e16bb2aacc3
Basil Hess <bhe@zurich.ibm.com>
2024-03-07 10:11:20 +0100

    fix comments in generate.sh [skip ci]

--- oqs-template/generate.sh
+++ oqs-template/generate.sh
@@ -2,8 +2,8 @@
 
 cd oqs-template
 
-# Step 2: Run the generator:
+# Step 1: Run the generator:
 cd .. && python3 oqs-template/generate.py
 
-# Step 3: Run clang-format.
+# Step 2: Run clang-format.
 find . -type f -and '(' -name '*.h' -or -name '*.c' -or -name '*.inc' ')' | xargs "${CLANG_FORMAT:-clang-format}" -i

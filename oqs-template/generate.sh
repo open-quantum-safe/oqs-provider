39791803399804036248b09267e59d6c7a557d2a
Basil Hess <bhe@zurich.ibm.com>
2024-03-07 10:09:33 +0100

    Fix OIDs for Kyber (r3)

--- oqs-template/generate.sh
+++ oqs-template/generate.sh
@@ -2,11 +2,6 @@
 
 cd oqs-template
 
-rm generate.yml
-
-# Step 1: Obtain current generate.yml from main:
-wget -c https://raw.githubusercontent.com/open-quantum-safe/openssl/OQS-OpenSSL_1_1_1-stable/oqs-template/generate.yml
-
 # Step 2: Run the generator:
 cd .. && python3 oqs-template/generate.py
 

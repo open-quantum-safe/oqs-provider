8a6a1cdd0aaa890735dceada81ecafd1926a589a
Basil Hess <bhe@zurich.ibm.com>
2024-03-07 12:46:39 +0100

    Set Kyber OIDs (#368)
    * Fix OIDs for Kyber (r3)
    
    * fix comments in generate.sh [skip ci]
    
    * run clang-format

--- oqs-template/generate.sh
+++ oqs-template/generate.sh
@@ -2,13 +2,8 @@
 
 cd oqs-template
 
-rm generate.yml
-
-# Step 1: Obtain current generate.yml from main:
-wget -c https://raw.githubusercontent.com/open-quantum-safe/openssl/OQS-OpenSSL_1_1_1-stable/oqs-template/generate.yml
-
-# Step 2: Run the generator:
+# Step 1: Run the generator:
 cd .. && python3 oqs-template/generate.py
 
-# Step 3: Run clang-format.
+# Step 2: Run clang-format.
 find . -type f -and '(' -name '*.h' -or -name '*.c' -or -name '*.inc' ')' | xargs "${CLANG_FORMAT:-clang-format}" -i

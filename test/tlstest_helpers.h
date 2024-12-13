f7228d214febb3f6848dfe80a464a2c7379f2edf
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2024-12-13 09:18:44 +0100

    Add DTLS 1.3 support (#586)
    * First cut at DTLS1.3 tracker
    
    Signed-off-by: Michael Baentsch <57787676+baentsch@users.noreply.github.com>
    
    * add separate DTLS object creation and testing
    
    Signed-off-by: Michael Baentsch <57787676+baentsch@users.noreply.github.com>
    
    * enables conditional use of DTLS1.3
    
    Signed-off-by: Michael Baentsch <57787676+baentsch@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Michael Baentsch <57787676+baentsch@users.noreply.github.com>

--- test/tlstest_helpers.h
+++ test/tlstest_helpers.h
@@ -4,9 +4,9 @@ int create_cert_key(OSSL_LIB_CTX *libctx, char *algname, char *certfilename,
                     char *privkeyfilename);
 
 int create_tls1_3_ctx_pair(OSSL_LIB_CTX *libctx, SSL_CTX **sctx, SSL_CTX **cctx,
-                           char *certfile, char *privkeyfile);
+                           char *certfile, char *privkeyfile, int dtls_flag);
 
 int create_tls_objects(SSL_CTX *serverctx, SSL_CTX *clientctx, SSL **sssl,
-                       SSL **cssl);
+                       SSL **cssl, int use_dgram);
 
 int create_tls_connection(SSL *serverssl, SSL *clientssl, int want);

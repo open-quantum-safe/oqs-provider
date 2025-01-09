9b6ff33c0d2faf99de598c96803aca837389bfb6
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2025-01-09 08:11:30 +0100

    limit libctx test to oqsprovider
    Signed-off-by: Michael Baentsch <57787676+baentsch@users.noreply.github.com>

--- test/oqs_test_libctx.c
+++ test/oqs_test_libctx.c
@@ -159,7 +159,7 @@ static int oqs_generate_kem_elems(const char *kemalg_name, EVP_PKEY **key,
 
     if (OSSL_PROVIDER_available(libctx, "default")) {
         testresult = (ctx = EVP_PKEY_CTX_new_from_name(libctx, kemalg_name,
-                                                       NULL)) != NULL &&
+                                                       "provider=oqsprovider")) != NULL &&
                      EVP_PKEY_keygen_init(ctx) && EVP_PKEY_generate(ctx, key);
 
         if (!testresult)

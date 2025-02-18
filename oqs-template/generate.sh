86ec1618d88c58a20104b7e39310ff0a58a4cae1
Robin Lösch <robin@chilio.net>
2025-02-18 15:04:31 +0100

    Only run clang-format c files owned by this repository
    Signed-off-by: Robin Lösch <robin@chilio.net>

--- oqs-template/generate.sh
+++ oqs-template/generate.sh
@@ -6,4 +6,5 @@ python3 oqs-template/generate.py
 
 # Step 2: Run clang-format.
 echo "Run ${CLANG_FORMAT:-clang-format}"
-find . -type f -and '(' -name '*.h' -or -name '*.c' -or -name '*.inc' ')' | xargs "${CLANG_FORMAT:-clang-format}" --style="{BasedOnStyle: llvm, IndentWidth: 4}" -i --Werror
+find oqsprov/ -type f -and '(' -name '*.h' -or -name '*.c' -or -name '*.inc' ')' | xargs "${CLANG_FORMAT:-clang-format}" --style="{BasedOnStyle: llvm, IndentWidth: 4}" -i --Werror
+find test/ -type f -and '(' -name '*.h' -or -name '*.c' -or -name '*.inc' ')' | xargs "${CLANG_FORMAT:-clang-format}" --style="{BasedOnStyle: llvm, IndentWidth: 4}" -i --Werror

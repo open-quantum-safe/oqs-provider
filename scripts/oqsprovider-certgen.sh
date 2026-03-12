def55c62c21b45ca72fc9560f93c51460173794d
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2026-03-12 19:42:12 +0100

    revert propquery usage
    Signed-off-by: Michael Baentsch <57787676+baentsch@users.noreply.github.com>

--- scripts/oqsprovider-certgen.sh
+++ scripts/oqsprovider-certgen.sh
@@ -30,9 +30,9 @@ echo "oqsprovider-certgen.sh commencing..."
 #rm -rf tmp
 mkdir -p tmp
 
-$OPENSSL_APP req -x509 -new -newkey $1 -propquery 'provider=oqsprovider' -keyout tmp/$1_CA.key -out tmp/$1_CA.crt -nodes -subj "/CN=oqstest CA" -days 365 && \
-$OPENSSL_APP genpkey -algorithm $1 -propquery 'provider=oqsprovider' -out tmp/$1_srv.key && \
-$OPENSSL_APP req -new -newkey $1 -propquery 'provider=oqsprovider' -keyout tmp/$1_srv.key -out tmp/$1_srv.csr -nodes -subj "/CN=oqstest server" && \
+$OPENSSL_APP req -x509 -new -newkey $1 -keyout tmp/$1_CA.key -out tmp/$1_CA.crt -nodes -subj "/CN=oqstest CA" -days 365 && \
+$OPENSSL_APP genpkey -algorithm $1 -out tmp/$1_srv.key && \
+$OPENSSL_APP req -new -newkey $1 -keyout tmp/$1_srv.key -out tmp/$1_srv.csr -nodes -subj "/CN=oqstest server" && \
 $OPENSSL_APP x509 -req -in tmp/$1_srv.csr -out tmp/$1_srv.crt -CA tmp/$1_CA.crt -CAkey tmp/$1_CA.key -CAcreateserial -days 365 -extensions usr_cert && \
 $OPENSSL_APP verify -CAfile tmp/$1_CA.crt tmp/$1_srv.crt
 # test PEM/DER/TEXT encoder/decoder logic:
